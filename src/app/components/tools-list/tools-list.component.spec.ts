adeda770f25dc0cffc6288034698a8ba81dec976
riteshporiya <riteshporiya124@gmail.com>
2024-07-06 15:59:34 +0530

    feat(character-count): implement character counter tool

---
+++ src/app/components/tools-list/tools-list.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ToolsListComponent } from './tools-list.component';
+
+describe('ToolsListComponent', () => {
+  let component: ToolsListComponent;
+  let fixture: ComponentFixture<ToolsListComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ToolsListComponent]
+    });
+    fixture = TestBed.createComponent(ToolsListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
